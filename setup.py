7a3ff4f59047279d0d0d4140c5796d3207db5cf9
Ian Liu Rodrigues <ian.liu88@gmail.com>
2015-05-04 18:10:36 -0300

    Add script to plot geometry

--- setup.py
+++ setup.py
@@ -6,4 +6,5 @@ setup(name='pymei',
         version='1.0',
         description='Python library for reading SU and SEG-Y files',
         py_modules=['pymei'],
+        scripts=['plotgeom'],
         )
