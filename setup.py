9a542e0973a145c42d19c81f033f4c6eda6667bc
Ian Liu Rodrigues <ian.liu88@gmail.com>
2015-05-04 17:31:31 -0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pymei',
+        version='1.0',
+        description='Python library for reading SU and SEG-Y files',
+        py_modules=['pymei'],
+        )
